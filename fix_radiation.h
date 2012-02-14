c420e51b6419034f087618747118fe3e95e09cad
Illia Izotov <izotov.ilya@gmail.com>
2012-02-14 23:38:10 +0100

    Tried to implement...

---
+++ fix_radiation.h
@@ -0,0 +1,35 @@
+/* 
+ * File:   fix_radiation.h
+ * Author: felix
+ *
+ * Created on February 14, 2012, 10:49 PM
+ */
+
+#ifdef FIX_CLASS
+
+FixStyle(heat/radiation,FixRadiation)
+
+#else
+
+#ifndef LMP_FIX_RADIATION_H
+#define	LMP_FIX_RADIATION_H
+
+#include "fix.h"
+
+namespace LAMMPS_NS {
+
+class FixRadiation : public Fix {
+ public:
+  FixRadiation(class LAMMPS *, int, char **);
+  ~FixRadiation();
+  int setmask();
+  void init(LAMMPS *);
+
+ private:
+     int temp;
+};
+
+}
+
+#endif
+#endif
